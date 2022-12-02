acf9869a091ce0cfdc3e51bd65d14b2deb82a5b2
Tijmen Vink <tijmen@brightin.nl>
2022-12-02 11:30:46 +0100

    Day 2 part 2

--- src/aoc/day_2.clj
+++ src/aoc/day_2.clj
@@ -1,36 +1,38 @@
-(ns aoc.day-2.main
+(ns aoc.day-2
   (:require [clojure.java.io :as io]))
 
 (def ^:private SHAPE-SCORES
   {\A 1 ;; Rock
    \B 2 ;; Paper
    \C 3 ;; Sciccors
-   \X 1 ;; Rock
-   \Y 2 ;; Paper
-   \Z 3 ;; Sciccors
    })
 
 (def ^:private GAME-SCORES
-  {"A X" 3
-   "A Y" 6
-   "A Z" 0
-   "B X" 0
-   "B Y" 3
-   "B Z" 6
-   "C X" 6
-   "C Y" 0
-   "C Z" 3})
+  {\X 0 ;; Lose
+   \Y 3 ;; Draw
+   \Z 6 ;; Win
+   })
+
+(def ^:private GAME-REACTIONS
+  {"A X" \C
+   "A Y" \A
+   "A Z" \B
+   "B X" \A
+   "B Y" \B
+   "B Z" \C
+   "C X" \B
+   "C Y" \C
+   "C Z" \A})
 
 (defn- score [[_ _ last :as line]]
-  (+ (get SHAPE-SCORES last)
-     (get GAME-SCORES line)))
+  (+ (get GAME-SCORES last)
+     (get SHAPE-SCORES (get GAME-REACTIONS line))))
 
 (defn- total-score [lines]
   (->> (map score lines)
        (apply +)))
 
 (comment
-  ;; Part 1
   (with-open [rdr (io/reader "resources/day_2/input.csv")]
     (->>
      (reduce conj [] (line-seq rdr))
